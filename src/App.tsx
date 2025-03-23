e7268d35c68b09d638324edcc1f13f31a190c8ca
maximisecoders <maxproductionsofficials@gmail.com>
2025-03-23 13:51:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
       <ToastProvider>
         <Toast />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Index />} />
